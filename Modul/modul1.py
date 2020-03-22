36568865854270cd7592830e45e58a6693d76cfe
AhmtOsmn <Ahmet.osmn@outlook.com>
2020-03-22 18:05:59 +0300

    modul1

---
+++ Modul/modul1.py
@@ -0,0 +1,12 @@
+programlama_dilleri = ["Python","Php","Java","C"]
+
+def toplama(a,b,c):
+    """
+    Bu fonksiyon 3 tane degerin toplamını döndürür.
+    """
+    return a + b + c
+def selamla(isim):
+    """
+    Bu fonksiyon ismi girilen kişiyi selamlar.
+    """
+    print("Selam",isim)
