58228379e7048f1894617e7193ff296706f49f2c
AhmtOsmn <Ahmet.osmn@outlook.com>
2020-03-22 18:06:06 +0300

    modul1_deneme

---
+++ Modul/modul1_deneme.py
@@ -0,0 +1,12 @@
+import modul1
+
+#print(modul1.toplama(15,20,25))
+
+#modul1.selamla("Ahmet")
+
+#print(modul1.programlama_dilleri)
+
+#print(modul1.programlama_dilleri[0])
+
+#eger her yerden kullanabilecegim bir modul yazmak istiyorsam,
+#pythonun klasoründen lib e atmalıyım.
